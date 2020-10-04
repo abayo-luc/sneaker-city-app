b53d64480387458f2c34176df65c935b4440e03f
Luc <luc.bayo@gmail.com>
2020-10-04 15:46:52 +0200

    feature: add product page

---
+++ src/componets/Loader/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default () => (
+  <div className="sonar-wrapper">
+    <div className="sonar-emitter">
+      <div className="sonar-wave"></div>
+    </div>
+  </div>
+);
--- src/pages/Product/index.js
+++ src/pages/Product/index.js
@@ -1,4 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Product from '../../componets/Product';
+import { useParams } from 'react-router-dom';
+import Axios from 'axios';
+import Loader from '../../componets/Loader';
+const { REACT_APP_API_BASE_URL } = process.env;
+export default () => {
+  const [loading, setLoading] = useState(false);
+  const [sneaker, setSnkear] = useState({});
+  const { id } = useParams();
 
-export default () => <Product />;
+  const fetchData = (id) => {
+    setLoading(true);
+    return Axios.get(`${REACT_APP_API_BASE_URL}/sneakers/${id}`)
+      .then((res) => {
+        const { sneaker } = res.data;
+        setSnkear(sneaker);
+      })
+      .catch((err) => {
+        console.log(err.response);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
+
+  useEffect(() => {
+    fetchData(id);
+  }, [id]);
+  if (loading) {
+    return <Loader />;
+  }
+  return <Product data={sneaker} />;
+};
